a3d27058cc110e9f9055c0f094d379225290324a
duythuan123 <146064542+duythuan123@users.noreply.github.com>
2024-10-24 21:46:56 +0700

    ENUM FOR STATUS AND ROLE

--- BusinessLayer/Utilities/MappingProfileExtension.cs
+++ BusinessLayer/Utilities/MappingProfileExtension.cs
@@ -3,11 +3,13 @@ using BusinessLayer.Models.Request;
 using BusinessLayer.Models.Response;
 using BusinessLayer.Services;
 using DataAccessLayer.Entities;
+using DataAccessLayer.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UberSystem.Domain.Enums;
 
 namespace BusinessLayer.Utilities
 {
@@ -17,15 +19,17 @@ namespace BusinessLayer.Utilities
         {
             CreateMap<User, UserResponseModel>();
             CreateMap<UserRequestModel, User>()
-                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => Helper.HashPassword(src.Password)));
+                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => Helper.HashPassword(src.Password)))
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => UserRole.USER.ToString()));
             CreateMap<UserRequestModelForUpdate, User>();
             CreateMap<Appointment, AppointmentResponseModel>();
-            CreateMap<AppointmentRequestModel, Appointment>();
+            CreateMap<AppointmentRequestModel, Appointment>()
+                 .ForMember(dest => dest.Status, opt => opt.MapFrom(src => AppointmentStatus.CREATED.ToString()));
             CreateMap<AppointmentRequestModelForUpdate, Appointment>();
             CreateMap<PaymentResponseModel, Payment>()
             .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => Helper.ExtractAmountFromOrderDescription(src.OrderDescription)))
             .ForMember(dest => dest.PaymentDate, opt => opt.MapFrom(src => DateTime.Now))
-            .ForMember(dest => dest.PaymentStatus, opt => opt.MapFrom(src => src.Success ? "Success" : "Failed"))
+            .ForMember(dest => dest.PaymentStatus, opt => opt.MapFrom(src => src.Success ? PaymentStatus.SUCCESS.ToString() : PaymentStatus.FAILED.ToString()))
             .ForMember(dest => dest.AppointmentId, opt => opt.MapFrom(src => Helper.ExtractAppointmentIdFromOrderDescription(src.OrderDescription)));
         }
     }
